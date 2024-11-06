620fe45093c6a4ededc3567f6ef4c33cdc22fa04
leticiajmaily18 <leticia.jamily@escolar.ifrn.edu.br>
2024-11-06 07:48:41 -0300

    Criando projeto de PDS

---
+++ src/main/java/ifrn/pi/ProjetoDPS/ProjetoDpsApplication.java
@@ -0,0 +1,13 @@
+package ifrn.pi.ProjetoDPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifrn/pi/ProjetoDPS/ProjetoDpsApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.pi.ProjetoDPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
