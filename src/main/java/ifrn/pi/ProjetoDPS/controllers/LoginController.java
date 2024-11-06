d2ed758781cc759c60683195a5d7f6d1972fc855
leticiajmaily18 <leticia.jamily@escolar.ifrn.edu.br>
2024-11-06 11:20:19 -0300

    Atualizando página de login e criando página de cadastro

--- src/main/java/ifrn/pi/ProjetoDPS/controllers/LoginController.java
+++ src/main/java/ifrn/pi/ProjetoDPS/controllers/LoginController.java
@@ -1,5 +1,19 @@
 package ifrn.pi.ProjetoDPS.controllers;
 
-public enum LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class LoginController {
+
+	@PostMapping("/mercado/cadastro")
+	public String cadastro() {
+		return "Cadastro";
+	}
+	
+	@PostMapping("/mercado/login")
+	public String login() {
+		return "Login";
+	}
 }
